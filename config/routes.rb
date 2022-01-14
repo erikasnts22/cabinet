9745d5241b1ed4bd88983e68b598b6f17ed866f1
“erikasnts22” <sntserika22@gmail.com>
2022-01-14 14:06:20 +0800

    Doc controller, model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
+  resources :docs
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
